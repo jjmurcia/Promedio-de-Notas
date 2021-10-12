537895a0ad82b5b091344746466f47db8711d0d1
Jhon Murcia <jjmurcia.r19@gmail.com>
2021-10-12 15:20:12 -0500

    News Styles

--- js/index.js
+++ js/index.js
@@ -4,15 +4,12 @@ function promedio() {
     var nota2 = parseFloat(document.getElementById("nota2").value);
     var nota3 = parseFloat(document.getElementById("nota3").value);
     var proceso = (nota1 + nota2 + nota3) / 3;
-
     console.log(proceso);
     var tabla = document.getElementById("tabla");
-
     if (nota1 > 5 || nota2 > 5 || nota3 > 5) {
         alert("Ud ingreso notas mayores a 5")
         tabla.deleteRows();
     }
-
     for (var j = 0; j < 1; j++) {
         var fila = tabla.insertRow(tabla.rows.label);
         var columna1 = fila.insertCell(0);
@@ -22,8 +19,6 @@ function promedio() {
         columna1.innerHTML = nombre;
         columna2.innerHTML = proceso;
     }
-
-
     if (proceso >= 3 && proceso <= 5) {
         columna3.innerHTML = "aprobado";
         columna3.style.backgroundColor = '#009846';
@@ -32,7 +27,6 @@ function promedio() {
         columna3.style.backgroundColor = '#FA5858';
     }
 }
-
 document.getElementById("saveDataBtn").addEventListener("click", function(e) {
     e.preventDefault()
     var nota1 = document.getElementById("nota1").value
@@ -46,5 +40,4 @@ document.getElementById("saveDataBtn").addEventListener("click", function(e) {
         localStorage.setItem(nota1, [nombre, "3", "4", "5"])
         alert("Todo quedó guardado!")
     }
-
 })
